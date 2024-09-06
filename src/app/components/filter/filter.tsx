1109d1c2f055a2d1e3e8b3cc2db2e3a6c8aabbdf
Elena Rybakova <lelkya84@gmail.com>
2024-09-06 18:12:32 +0300

    filter

--- src/app/components/filter/filter.tsx
+++ src/app/components/filter/filter.tsx
@@ -1,26 +1,109 @@
+"use client";
+
 import classNames from "classnames";
 import styles from "./filter.module.css";
-
+import { useState } from "react";
 
 export default function Filter() {
+  const [isOpen, setIsOpen] = useState({ isOp: false, filter: 0 });
+
+  function handleFiltChange(type: number) {
+    if (isOpen.filter === type) {
+      setIsOpen({ isOp: !isOpen.isOp, filter: type });
+    } else {
+      setIsOpen({ isOp: true, filter: type });
+    }
+  }
+
+  const arr1 = [
+    "Michael Jackson",
+    "Frank Sinatra",
+    "Calvin Harris",
+    "Zhu",
+    "Arctic Monkeys",
+  ];
+  const arr2 = ["Рок", "Хип-хоп", "Поп-музыка", "Техно", "Инди"];
+  const arr3 = ["По умолчанию", "Сначала новые", "Сначала старые"];
+
   return (
     <div className={classNames(styles.centerblock__filter, styles.filter)}>
       <div className={styles.filter__title}>Искать по:</div>
-      <div
-        className={classNames(
-          styles.filter__button,
-          styles.buttonAuthor,
-          styles._btnText
+      <div>
+        <button
+          className={classNames(
+            styles.filter__button,
+            styles.buttonAuthor,
+            styles._btnText
+          )}
+          onClick={() => handleFiltChange(1)}
+        >
+          исполнителю
+        </button>
+        {isOpen.isOp && isOpen.filter === 1 && (
+          <div className={styles.filt}>
+            {arr1.map((el, id) => {
+              return (
+                <button
+                  key={id}
+                  className={styles.filt__item}
+                  onClick={() => console.log(el, id)}
+                >
+                  {el}
+                </button>
+              );
+            })}
+          </div>
         )}
-      >
-        исполнителю
       </div>
-      <div className={classNames(styles.filter__button, styles._btnText)}>
-        году выпуска
+      <div>
+        <button
+          className={classNames(styles.filter__button, styles._btnText)}
+          onClick={() => handleFiltChange(2)}
+        >
+          году выпуска
+        </button>
+        {isOpen.isOp && isOpen.filter === 2 && (
+          <div className={styles.filt}>
+            {arr2.map((el, id) => {
+              return (
+                <button
+                  key={id}
+                  className={styles.filt__item}
+                  onClick={() => {
+                    console.log(el, id);
+                  }}
+                >
+                  {el}
+                </button>
+              );
+            })}
+          </div>
+        )}
       </div>
-      <div className={classNames(styles.filter__button, styles._btnText)}>
-        жанру
+      <div>
+        <button
+          className={classNames(styles.filter__button, styles._btnText)}
+          onClick={() => handleFiltChange(3)}
+        >
+          жанру
+        </button>
+        {isOpen.isOp && isOpen.filter === 3 && (
+          <div className={styles.filt}>
+            {arr3.map((el, id) => {
+              return (
+                <button
+                  onClick={() => console.log(el)}
+                  key={id}
+                  className={styles.filt__item}
+                >
+                  {el}
+                </button>
+              );
+            })}
+          </div>
+        )}
       </div>
+      <></>
     </div>
   );
 }
