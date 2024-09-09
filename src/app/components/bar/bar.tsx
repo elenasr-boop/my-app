396327ae666eb217da530b6e12701c0a3c5f9e8b
Elena Rybakova <lelkya84@gmail.com>
2024-09-09 21:34:54 +0300

    play/pause

--- src/app/components/bar/bar.tsx
+++ src/app/components/bar/bar.tsx
@@ -1,4 +1,4 @@
-'use client';
+"use client";
 
 import { useCurrentTrack } from "@/app/context/CurrentTrackProvider";
 import styles from "./bar.module.css";
@@ -7,28 +7,50 @@ import { TrackPlay } from "../trackPlay/trackPlay";
 import { Volume } from "../volume/volume";
 import { PlayerControl } from "../playerControl/playerControl";
 import { ProgressBar } from "../progressBar/progressBar";
+import { useEffect, useRef, useState } from "react";
 
-export default function Bar () {
-  const {currentTrack} = useCurrentTrack();
+export default function Bar() {
+  const { currentTrack } = useCurrentTrack();
+  const [isPlaying, setIsPlaying] = useState(true);
+  const audioRef = useRef(null);
 
-  if(!currentTrack) {
+  useEffect(() => {
+    if (currentTrack) {
+      const audio = audioRef.current;
+      audio.play();
+    }
+  }, [currentTrack]);
+
+  if (!currentTrack) {
     return null;
   }
 
-  const {name, author} = currentTrack;
+  const { name, author, track_file } = currentTrack;
 
-    return (
-        <div className={styles.bar}>
-          <div className={styles.bar__content}>
-            <ProgressBar />
-            <div className={styles.bar__playerBlock}>
-              <div className={classNames(styles.bar__player, styles.player)}>
-                <PlayerControl />
-                <TrackPlay name={name} author={author} />
-              </div>
-              <Volume />
-            </div>
+  function playPauseTrack() {
+    const audio = audioRef.current;
+    if (isPlaying) {
+      audio.pause();
+    } else {
+      audio.play();
+    }
+
+    setIsPlaying(!isPlaying);
+  }
+
+  return (
+    <div className={styles.bar}>
+      <audio ref={audioRef} src={track_file} className={styles.audio} />
+      <div className={styles.bar__content}>
+        <ProgressBar />
+        <div className={styles.bar__playerBlock}>
+          <div className={classNames(styles.bar__player, styles.player)}>
+            <PlayerControl isPlaying={isPlaying} setIsPlaying={playPauseTrack} />
+            <TrackPlay name={name} author={author} />
           </div>
+          <Volume />
         </div>
-    )
+      </div>
+    </div>
+  );
 }
--- src/app/components/playerControl/playerControl.tsx
+++ src/app/components/playerControl/playerControl.tsx
@@ -1,7 +1,14 @@
 import classNames from "classnames";
 import styles from "./playerControl.module.css";
+import { Dispatch, SetStateAction } from "react";
+import Image from "next/image";
 
-export function PlayerControl() {
+type PlayerControlProps = {
+  isPlaying: boolean;
+  setIsPlaying: Dispatch<SetStateAction<boolean>>;
+};
+
+export function PlayerControl({ isPlaying, setIsPlaying }: PlayerControlProps) {
   return (
     <div className={styles.player__controls}>
       <div className={styles.player__btnPrev}>
@@ -9,10 +16,20 @@ export function PlayerControl() {
           <use xlinkHref="img/icon/sprite.svg#icon-prev"></use>
         </svg>
       </div>
-      <div className={classNames(styles.player__btnPlay, styles._btn)}>
-        <svg className={styles.player__btnPlaySvg}>
-          <use xlinkHref="img/icon/sprite.svg#icon-play"></use>
-        </svg>
+      <div
+        className={classNames(styles.player__btnPlay, styles._btn)}
+        onClick={() => {
+          setIsPlaying(!isPlaying);
+        }}
+      >
+        {!isPlaying && (
+          <svg className={styles.player__btnPlaySvg}>
+            <use xlinkHref="img/icon/sprite.svg#icon-play"></use>
+          </svg>
+        )}
+        {isPlaying && (
+          <Image className={styles.player__btnPlaySvg} src="img/icon/pause.svg" alt="" width={22} height={20} />
+        )}
       </div>
       <div className={styles.player__btnNext}>
         <svg className={styles.player__btnNextSvg}>
